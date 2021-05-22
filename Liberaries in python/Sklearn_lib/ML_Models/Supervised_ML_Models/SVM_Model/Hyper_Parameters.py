be847dc1e92facaa56775a59c4042c1a61d9cbe3
Ahmed <ahmed.elsayed862@yahoo.com>
2021-05-22 06:19:15 +0200

    Hyper Parameters

---
+++ Liberaries in python/Sklearn_lib/ML_Models/Supervised_ML_Models/SVM_Model/Hyper_Parameters.py
@@ -0,0 +1,12 @@
+Hyper_Paramters of SVM ::
+
+----------------------------
+----------------------------
+
+1 - max_iter = any_num
+
+2 - Tol = (0.0001 , 0.001 , 0.01 , 0.1)
+
+3 - c = (1.0 , 10 , 20 , 50)  any_num
+
+4 - kernel = ( "linear" , "rbf" , "precomputed" , "poly" , "sigmoid")
